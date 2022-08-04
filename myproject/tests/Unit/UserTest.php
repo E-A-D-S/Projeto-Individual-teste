3f9da142b4a99e9552ddfea80e2499cebb5a39d1
Eduardo Araújo dos Santos <eduardoeko7@gmail.com>
2022-08-04 06:03:58 -0300

    projeto ok faltando os testes

---
+++ myproject/tests/Unit/UserTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+  
+    public function test_user()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_create_user()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_edit_user()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_update_user()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function test_delete_user()
+    {
+        $this->assertTrue(true);
+    }
+}
